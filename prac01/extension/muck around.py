fe9e5631d06f9603ee017e12342caa65cc0617c7
donaldCull <donaldjames.cull@my.jcu.edu.au>
2016-08-12 18:45:46 +1000

    Delete muck around.py

--- prac01/extension/muck around.py
+++
@@ -1,8 +0,0 @@
-# class encapsulates data and functionality
-# shift f6 refactor variable name rename
-# can use python interpreter to test code
-# exam what does this code output
-# shift enter goes straight to the next line
-
-# shift tab dedent
-
