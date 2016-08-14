3ad8c6a1ca1767df983ac16e1a41b4e59a2b93d3
donaldCull <donaldjames.cull@my.jcu.edu.au>
2016-08-14 22:01:55 +1000

    program segmented into separate functions

--- prac03/second_letters_in_name.py
+++ prac03/second_letters_in_name.py
@@ -1,12 +1,23 @@
 __author__ = 'Donald Cull'
 
 def main():
+    name, name_length = get_name()
+    display_name(name, name_length)
+
+
+def display_name(name, name_length):
+    for char in range(name_length):
+        if char % 2 == 0:
+            print(name[char])
+
+
+def get_name():
     name = input("Enter your name: ")
     name_length = len(name)
     while name_length == 0:
         name = input("Enter your name again: ")
         name_length = len(name)
-    for char in range(name_length):
-        if char % 2 == 0:
-            print(name[char])
+    return name, name_length
+
+
 main()
