914d01d34c59709af9e617e4e2d0571fe7bf30dd
donaldCull <donaldjames.cull@my.jcu.edu.au>
2016-08-21 22:13:52 +1000

    quickpick can produce random numbers but list length varies

--- prac04/quickpick.py
+++ prac04/quickpick.py
@@ -1,7 +1,16 @@
 __author__ = 'Donald Cull'
 import random
 number_of_quickpicks = int(input("How many quickpicks?"))
+quick_pick_row = []
+
 for i in range(number_of_quickpicks):
     print("\n")
     for i in range(6):
-        print(random.randrange(1, 45, 1),end=" ")
+        quick_number = random.randint(1,45)
+        if quick_number in quick_pick_row:
+            quick_number = random.randint(1, 45)
+        else:
+            quick_pick_row.append(quick_number)
+    print(quick_pick_row)
+    quick_pick_row.clear()
+
