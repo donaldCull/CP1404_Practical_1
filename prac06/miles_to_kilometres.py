59f50e5e51fc9858d622d6b5f16451a3d3487536
donaldcull <donaldjames.cull@my.jcu.edu.au>
2016-09-12 07:16:14 +1000

    miles_to_kilometres handles incorrect inputs and displays the correct output

--- prac06/miles_to_kilometres.py
+++ prac06/miles_to_kilometres.py
@@ -10,6 +10,8 @@ from kivy.core.window import Window
 
 __author__ = 'Donald Cull'
 
+MILES_TO_KILOMETRES = 1.60934
+
 
 class MilesToKilometresApp(App):
     """MilesToKilometresApp is a Kivy App to convert Miles to Kilometres """
@@ -19,13 +21,23 @@ class MilesToKilometresApp(App):
         self.title = "Miles to Kilometres"
         self.root = Builder.load_file('miles_to_kilometres.kv')
         return self.root
-    def handle_calculate(self, value):
-        result = value * 1.60934
-        self.root.ids.output_number.text = str("{:.3f}".format(result))
-    def handle_increment(self, value):
-        positive_increment = value + 1
-        self.root.ids.input_number.text = int(positive_increment)
 
+    def handle_calculate(self):
+        value = self.get_valid_miles()
+        result = value * MILES_TO_KILOMETRES
+        self.root.ids.output_number.text = str(result)
+
+    def handle_increment(self, increment):
+        value = self.get_valid_miles() + increment
+        self.root.ids.input_number.text = str(value)
+        self.handle_calculate()
+
+    def get_valid_miles(self):
+        try:
+            value = float(self.root.ids.input_number.text)
+            return value
+        except ValueError:
+            return 0
 
 
 MilesToKilometresApp().run()
